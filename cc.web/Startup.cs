79000d138249efd4e23773fa26f0933c4c491dca
liuyf <liuyf@irisking.com>
2019-12-13 15:49:24 +0800

    启用静态页

--- cc.web/Startup.cs
+++ cc.web/Startup.cs
@@ -40,7 +40,10 @@ namespace cc.web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseDefaultFiles();
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(defaultFilesOptions);
             app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
